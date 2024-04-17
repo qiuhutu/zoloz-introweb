f51694a5059bd57fe8dc8ba7c7fd827e4f989734
Yu1, Yue <Yue.Yu1@sc.com>
2024-04-17 12:17:40 +0800

    ADO#3750379 page init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import selfieImage from './selfie_intro.svg';
+import arrowLeft from './arrow_left.svg';
 import './App.css';
+import ZolozRealIdCore from './zolozReadIdCore';
+
+const langPack = {
+  system_error_title: "System Error",
+  system_error_msg: 'Please try again later',
+  system_error_got_it: 'Got it',
+  network_error_title: 'No Internet Connection',
+  network_error_msg: 'Please check your internet connection and try again',
+  network_error_got_it: 'Got it',
+  fe_retry_max_title: '',
+  fe_retry_max_msg: 'Sorry, you have too many failed identification attempts. Please try again later.',
+  fe_retry_max_got_it: 'Got it'
+};
+
+var zolozRealIdCore = new ZolozRealIdCore({})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="zoloz-page">
+      <div className="zoloz-topbar">
+        <img src={arrowLeft} className="zoloz-icon-arrow-left" alt="turnBack" />
+        <header className="zoloz-topbar-title">Scan Face</header>
+      </div>
+      <div>
+        <img src={selfieImage} className="App-logo" alt="logo" />
+      </div>
+      <footer className="zoloz-bottomBar">
+        Continue
+      </footer>
     </div>
   );
 }
